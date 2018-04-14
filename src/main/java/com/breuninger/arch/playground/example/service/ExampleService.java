8c8b443f99e2b77096515770a53010b1b8056747
Benedikt Stemmildt <benedikt@stemmildt.com>
2018-04-14 13:17:47 +0200

    fix: Example count gauge metric

--- src/main/java/com/breuninger/arch/playground/example/service/ExampleService.java
+++ src/main/java/com/breuninger/arch/playground/example/service/ExampleService.java
@@ -2,21 +2,28 @@ package com.breuninger.arch.playground.example.service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import com.breuninger.arch.playground.example.domain.Example;
 import com.breuninger.arch.playground.example.domain.ExampleRepository;
-import com.codahale.metrics.annotation.Gauge;
 
-import lombok.AllArgsConstructor;
+import io.micrometer.core.instrument.MeterRegistry;
 
-@AllArgsConstructor
 @Service
 public class ExampleService {
 
   private final ExampleRepository exampleRepository;
+  private final LongAdder exampleCount;
+
+  public ExampleService(final ExampleRepository exampleRepository, final MeterRegistry meterRegistry) {
+    this.exampleRepository = exampleRepository;
+    exampleCount = new LongAdder();
+    exampleCount.add(count());
+    meterRegistry.gauge("example.count", exampleCount);
+  }
 
   public List<Example> findAll() {
     return exampleRepository.findAll();
@@ -24,15 +31,16 @@ public class ExampleService {
 
   public Example create(final Example example) {
     final var creationDate = new Date();
-    return exampleRepository.create(example.toBuilder()
+    final var createdExample = exampleRepository.create(example.toBuilder()
       .id(ObjectId.get().toString())
       .creationDate(creationDate)
       .lastModificationDate(creationDate)
       .build());
+    exampleCount.increment();
+    return createdExample;
   }
 
-  @Gauge(name = "example.count", absolute = true)
-  public long count() {
+  private long count() {
     return exampleRepository.size();
   }
 }
